47d82c41e756df2cf5aebb7afb866740f35fa669
Neha Agarwal <nehaagarwal@microsoft.com>
2023-08-30 17:21:39 -0700

    revert cleanup change

--- build-in-container/scripts/build-mariner.sh
+++ build-in-container/scripts/build-mariner.sh
@@ -78,7 +78,7 @@ source /mariner/scripts/setup.sh
 
 if [[ "${container_type}" == "build" ]]; then
     # exit if SPECS/ is empty
-    if [ ! "$(ls -A $SPECS_DIR)" ]; then exit 1; fi
+    if [ ! "$(ls -A $SPECS_DIR)" ]; then exit; fi
     source /mariner/scripts/build.sh
 else
     /bin/bash
